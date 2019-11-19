0ec2c3ab99da748f9a1b1caefb169bf5733f9dea
Massimo Colavita <30273901+Massii@users.noreply.github.com>
2019-11-19 04:42:05 -0300

    Update borrador.m

--- borrador.m
+++ borrador.m
@@ -13,22 +13,25 @@ h_d = [1 3510 1.004*10^7];
 h = (h_n*s_vec)/(h_d*s_vec);
 
 syms q t
-F = -q^2/(q^2 + 3510*q + 1.004*10^7);
+F = q^2/(q^2 + 3510*q + 1.004*10^7);
 y = ilaplace(F, q, t);
 
+
 %Calculo con Wolfram, Matlab y "manualmente" la antitransformada de H
-%H = -s^2/(s^2 + 3150*s + 1.004*10^7)
+%H = s^2/(s^2 + 3150*s + 1.004*10^7)
 %Obtengo la misma respuesta con Wolfram que  manualmente
 %
-%Wolfram
-figure()
-title('Wolfram')
-%-δ(t) + (1755 - 735.347*i)*e^((-1755 - 2638.18*i)*t)*((0.701314 + 0.712852*i) + e^((5276.35 i)*t))
-
 t = 0:0.0001:0.008;
-zw = -dirac(t) + (1755 - 735.347*i)*e.^((-1755 - 2638.18*i)*t).*((0.701314 + 0.712852*i) + e.^((5276.35*i)*t));
+%Wolfram
+zw = dirac(t) - (1755 - 735.347*i)*e.^((-1755 - 2638.18*i)*t).*((0.701314 + 0.712852*i) + e.^((5276.35*i)*t));
 
-plot(t,zw);
+figure()
+ plot(t,zw);
+ title('Wolfram');
+%δ(t) - (1755 - 735.347*i)*e^((-1755 - 2638.18*i)*t)*((0.701314 + 0.712852*i) + e^((5276.35 i)*t))
+
+
+
 
 %Matlab ---------------MAL LOS DATOS-- USE a = 3150 en lugar de 3510--------- Borrar los %%
 %%figure()
@@ -45,11 +48,26 @@ plot(t,zw);
 %%%
 %Calculo con wolfram la forma general de la antitransformada de 
 %xh = s^2/(s^2 + a*s + b) = s^2/((s-p1)(s-p2)), es zp donde p1 y p2 son los polos de h;
-% Linv {-s^2/((s-a)(s-b))} = -{(a^2*e^(a*t))/(a - b) - (b^2*e^(b*t))/(a - b) + δ(t)}
-figure()
-title('Manual')
+% Linv {s^2/((s-a)(s-b))} = δ(t) + (a^2*e^(a*t))/(a - b) - (b^2*e^(b*t))/(a - b)
 poles = [roots(h_d)]';
 p1 = poles(1);
 p2 = poles(2);
-zp = -(p1^2/(p1-p2))*e.^(p1*t) + (p2^2/(p1-p2))*e.^(p2*t) - dirac(t);
-plot(t,zp);
+zp = dirac(t) + (p1^2/(p1-p2))*e.^(p1*t) - (p2^2/(p1-p2))*e.^(p2*t);
+
+figure()
+ plot(t,zp);
+ title('Manual');
+
+
+
+%%%RESPUESTA AL ESCALON 
+%H = h
+%si u = escalon entonces U = 1/s entonces Y = H*U = H/s
+%Calculo con wolfram la forma general de la antitransformada de Y
+%Y = s/(s^2 + a*s + b) = s/((s-p1)(s-p2)), es zp donde p1 y p2 son los polos de h;
+% Linv {s/((s-a)(s-b))} = (a*e^(a*t))/(a - b) - (b*e^(b*t))/(a - b)
+z_step = (p1/(p1-p2))*e.^(p1*t) - (p2/(p1-p2))*e.^(p2*t);
+figure()
+ plot(t,z_step);
+ title('Respuesta al escalón');
+ axis([0 0.003])
